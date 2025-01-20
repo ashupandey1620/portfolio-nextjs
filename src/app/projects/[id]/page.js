966478effa368f9aa209bd1efecdcd50d444b940
Ashutosh Pandey <ashupandey1620@gmail.com>
2025-01-21 01:35:05 +0530

    Updates Pics

--- src/app/projects/[id]/page.js
+++ src/app/projects/[id]/page.js
@@ -2,11 +2,12 @@
 import { useRouter } from 'next/navigation';
 
 
+
 const ProjectDescription = ({params}) => {
     console.log(params)
     // const router = useRouter();
     // const { id } = router.query; // Get the dynamic ID from the URL
-const id=params.id
+    const id=params.id
 
     console.log(`this is the ${id}`)
 
@@ -23,7 +24,7 @@ const id=params.id
     return (
         <div className="container mx-auto px-5 py-10">
             <h1 className="text-white text-4xl font-bold">{project.title}</h1>
-            <img src={project.image} alt={project.title} className="w-full h-64 object-cover rounded-md mt-4" />
+            <img src={project.image} alt={project.title} className="w-50 h-80 object-cover rounded-b-md mt-4" />
             <p className="text-gray-300 mt-6">{project.description}</p>
 
             <div className="mt-8">
@@ -42,8 +43,6 @@ const id=params.id
     );
 };
 
-
-
 const projectsData = [
     {
         id: 1,
@@ -51,14 +50,14 @@ const projectsData = [
         description: "A cloud storage app similar to Google Drive, featuring file upload, download, and local storage integration. It utilizes NestJS and Node.js for backend services, Kotlin Jetpack Compose for the Android interface, PostgreSQL for database management, and AWS S3 for secure file storage, all hosted on AWS EC2 for optimal performance and scalability.",
         image: "/projects/github.png",
         tag: ["All", "Full Stack","Android", "Cloud", "Nest","PostgreSQL"],
-        gitUrl: "/",
+        gitUrl: "www.github.com",
         previewUrl: "/",
     },
     {
         id: 2,
         title: "Validator",
         description: "Validator is an application designed for Professors and Guards in a college environment to authenticate student-issued cards. Leveraging NFC technology, it enables seamless card validation, improving authentication speed by 250%.",
-        image: "/projects/github.png",
+        image: "/projects/validator.png",
         tag: ["All", "Backend","Android","NFC","IOT","Django","PostgreSQL"],
         gitUrl: "/",
         previewUrl: "/",
@@ -67,7 +66,7 @@ const projectsData = [
         id: 3,
         title: "Ignitia 2k24",
         description: "Ignitia - 2k24 is an Android app for a fest, enabling event registration and in-app purchases. Built with Kotlin Jetpack Compose and Django, it features secure hosting on AWS and robust OTP verification, enhancing user engagement and monetization.",
-        image: "/projects/github.png",
+        image: "/projects/ignitia_collage.png",
         tag: ["All", "Full Stack","Android","Backend", "Ecommerce", "Wallet", "Django","PostgreSQL"],
         gitUrl: "/",
         previewUrl: "/",
@@ -85,25 +84,16 @@ const projectsData = [
         id: 5,
         title: "Behance",
         description: "A Behance clone website featuring a modern and responsive design, built with React for the frontend and styled using Tailwind CSS. The backend is powered by Node.js and Express.js, providing a robust API for user authentication, project uploads, and interaction functionalities, creating an engaging platform for creatives to showcase their work.",
-        image: "/projects/github.png",
+        image: "/img.png",
         tag: ["All", "Web","React", "Tailwind CSS", "NodeJS + ExpressJS","MongoDB"],
         gitUrl: "/",
         previewUrl: "/",
     },
     {
         id: 6,
-        title: "Farragut",
-        description: "Farragut is a full-stack web application designed to manage a database of employees, developed using Next.js and Supabase. It streamlines task assignments and wage tracking to enhance productivity. The application features a responsive design with Tailwind CSS and incorporates data visualization tools like Chart.js to provide valuable insights into employee performance and task completion.",
-        image: "/projects/github.png",
-        tag: ["All", "Web","Full Stack","Firebase","Management Workflow"],
-        gitUrl: "/",
-        previewUrl: "/",
-    },
-    {
-        id: 7,
         title: "Stocks Application",
-        description: "",
-        image: "/projects/github.png",
+        description: "A Stocks Tracker App built with Jetpack Compose** and Kotlin, leveraging the Stock Free API to provide real-time stock market updates. This app offers a sleek and modern UI, enabling users to track stock prices, view detailed stock information, and monitor market trends. Designed for efficiency, the app ensures smooth navigation, real-time data syncing, and offline access to favorite stocks. It’s a reliable tool for both casual users and active investors to stay informed on the go.",
+        image: "/projects/img.png",
         tag: ["All", "Android","Stocks API","Android","Kotlin","Jetpack Compose"],
         gitUrl: "/",
         previewUrl: "/",
